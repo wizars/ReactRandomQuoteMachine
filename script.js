28ae9ce58969e37361875a3d5d6c45f72dbf8590
wizars <wizars@gmail.com>
2022-04-07 07:59:40 +0200

    test

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 import React from "https://cdn.skypack.dev/react@17.0.1";
 import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return
+<p> this is my first React project</p>
+        
+    }
+}
+ReactDOM.render(App,document.getElementById("body"))
